fbbf21bb5d33531e4aaef7bb7038996b5269ecd0
Akinbobola <folarin.akinbobola@gmail.com>
2024-02-12 01:03:40 +0800

    removed virtual

--- pos/settings.py
+++ pos/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oy&$uxkrsdom_#3((ve&7mgmx^l@*7%58yto^$ol$0)+@(oxj+'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False").lower == "true"
 
-ALLOWED_HOSTS =  ['firstbukkofpos.onrender.com']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -87,14 +88,13 @@ WSGI_APPLICATION = 'pos.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-        'default': {'ENGINE': 'django.db.backends.postgresql',
-        'NAME' : 'firstposother2',
-        'USER' : 'postgres',
-        'PASSWORD' :'644215',
-        'HOST' : 'localhost',
-        'PORT' : '5433'
+        'default': {'ENGINE': 'django.db.backends.sqlite3',
+        'NAME' : 'BASE_DIR /'"db.sqlite3",
+        
         }
 }
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 
