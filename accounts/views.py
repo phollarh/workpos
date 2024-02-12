be6327210af937d46728eb23a412839a8861379c
Akinbobola <folarin.akinbobola@gmail.com>
2024-02-13 04:38:09 +0800

    updated project

--- accounts/views.py
+++ accounts/views.py
@@ -424,7 +424,7 @@ def OutletCreatView(request):
 				
 				form_create.save()
 				messages.success(request, ('Outlet added successfully...'))
-				return redirect('/')
+				return redirect('settings_paged')
 
 		context={
 					'form':form,
